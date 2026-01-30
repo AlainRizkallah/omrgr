88a2fc659f417d0305c697cfa1117cb7e4e33b12
alain <alain_riz@hotmail.com>
2026-01-30 15:34:12 +0100

    Add .vercelignore to exclude Pictures folder and update next.config.mjs for output file tracing exclusions

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  experimental: {
+    outputFileTracingExcludes: {
+      "*": ["public/**"],
+    },
+  },
   images: {
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
